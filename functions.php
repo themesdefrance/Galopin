8e016c73f9619870c2c7bbfa138ade12fd3cb69f
Yannick Lohse <glorf.travian@gmail.com>
2014-08-16 13:48:06 +0200

    Added typicons

--- functions.php
+++ functions.php
@@ -2,4 +2,5 @@
 
 $theme = wp_get_theme();
 wp_enqueue_style('stylesheet', get_stylesheet_directory_uri().'/style.css', array(), $theme->get('Version'));
+wp_enqueue_style('icons', get_stylesheet_directory_uri().'/fonts/typicons.min.css', array(), $theme->get('Version'));
 register_nav_menu('primary', __('Main menu', 'galopin'));
