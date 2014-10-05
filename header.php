0f19f08885308ed1fcb0f85a3c00ae355c5d4e01
Yannick Lohse <glorf.travian@gmail.com>
2014-10-05 18:15:04 +0200

    Proper masonry color

--- header.php
+++ header.php
@@ -1,3 +1,4 @@
+<?php $masonry = is_home() && get_option('galopin_masonry'); ?>
 <!doctype html>
 <!--[if lt IE 7]> <html class="lt-ie9 lt-ie8 lt-ie7" <?php language_attributes(); ?>> <![endif]-->
 <!--[if IE 7]>    <html class="lt-ie9 lt-ie8" <?php language_attributes(); ?>> <![endif]-->
@@ -57,7 +58,7 @@
 				</ul>
 			</header>
 			
-			<div class="content">
+			<div class="content <?php if ($masonry) echo 'masonry-wrapper'; ?>">
 				<div class="hero-image" style="background-image: url(<?php echo get_option('galopin_hero_image'); ?>);">
 					<button class="menu-toggle typcn typcn-th-menu"></button>
 
