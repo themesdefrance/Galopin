b02413abe596670f76f0ede25f23a038d6a75131
alex_borto <themesdefrance@users.noreply.github.com>
2014-10-07 10:42:37 +0200

    Some fixes

--- content-body.php
+++ content-body.php
@@ -10,7 +10,7 @@ else if(is_category() || is_tax()){
 }else if(is_tag()|| is_search()){
 	echo galopin_excerpt(0); // == No excerpt
 }else{
-	echo galopin_excerpt(50);
+	echo galopin_excerpt(40);
 ?>
 	<a href="<?php the_permalink(); ?>" class="button" title="<?php the_title(); ?>"><?php _e('Read more','galopin'); ?></a>
 
--- index.php
+++ index.php
@@ -12,27 +12,47 @@ $sidebar = get_option('galopin_show_sidebar');
 		<div class="<?php if (!$masonry && $sidebar) echo 'col-2-3'; ?>">
 		
 			<ul class="posts <?php if ($masonry) echo 'masonry'; ?>">
+			
 				<?php while (have_posts()) : the_post(); ?>
+				
 				<li class="<?php if ($masonry) echo 'brick'; ?>">
+				
 					<?php get_template_part('content', get_post_format()); ?>
+					
 				</li>
+				
 				<?php endwhile; ?>
+				
 			</ul>
+			
 			<div class="pagination">
 				<?php galopin_posts_nav(false, ''); ?>
 			</div>
+			
 		</div>
+		
 		<?php if (!$masonry && $sidebar){ ?>
+		
 		<aside class="sidebar col-1-3">
+		
 			<?php get_sidebar('blog'); ?>
+			
 		</aside>
+		
 		<?php } ?>
+		
 	</div>
 
 	<aside class="footerbar">
+	
 		<div class="grid">
+		
 			<?php get_sidebar('footer'); ?>
+			
 		</div>
+		
 	</aside>
+	
 </div>
+
 <?php get_footer(); ?>
