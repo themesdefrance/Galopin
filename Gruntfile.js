d214553d06951947e6d4c25e11db794099928b99
alex_borto <themesdefrance@users.noreply.github.com>
2014-10-28 09:40:57 +0100

    Préparation release

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,10 +5,19 @@ module.exports = function(grunt) {
 			options: {},
 			dev: {
 				files: {
-					'./style.css': 'css/style.scss'
+					'./style.css': 'css/style.scss',
+					'css/style.css': 'css/style.scss'
 				},
 				options: {
 					outputStyle: 'nested'
+				}
+			},
+			build: {
+				files: {
+					'./style.css': 'css/style.scss',
+				},
+				options: {
+					outputStyle: 'compressed'
 				}
 			}
 		},
@@ -25,11 +34,20 @@ module.exports = function(grunt) {
 				tasks: ['compile']
 			},
 		},
+		copy: {
+			build: {
+				expand: true,
+				src: ['**', '!.sass-cache', '!Galopin/', '!node_modules/**', '!Gruntfile.js', '!README.md', '!package.json'],
+				dest: 'Galopin/',
+			}
+		}
 	});
 	
 	grunt.loadNpmTasks('grunt-sass');
 	grunt.loadNpmTasks('grunt-autoprefixer');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 	
 	grunt.registerTask('compile', ['sass:dev', 'autoprefixer:dev']);
+	grunt.registerTask('build', ['sass:dev', 'sass:build', 'autoprefixer:dev', 'copy:build']);
 };
