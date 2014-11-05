f5b36e90c680f70a59d8412ddd5342f4238be038
alex_borto <themesdefrance@users.noreply.github.com>
2014-11-05 15:15:54 +0100

    Titre masonry pour archives par auteur

--- header-masonry.php
+++ header-masonry.php
@@ -17,6 +17,14 @@
 			<h1 class="masonry-header-title">
 				<?php printf( __( 'Search results for : %s', 'galopin' ), get_search_query() ); ?>
 			</h1>
+			
+	<?php }else if(is_author()){ ?>
+	
+			<?php $author = (isset($_GET['author_name'])) ? get_user_by('slug', $author_name) : get_userdata(intval($author)); ?>
+			
+			<h1 class="masonry-header-title">
+				<?php printf( __( 'Posts by %s', 'galopin' ), $author->display_name ); ?>
+			</h1>
 		
 	<?php }else if(is_archive()){ ?>
 	
