60795017afa2aa65c4edd8d8dbbc3b8fc64f696e
alex_borto <themesdefrance@users.noreply.github.com>
2014-11-05 15:24:42 +0100

    Better font loading

--- functions.php
+++ functions.php
@@ -111,7 +111,9 @@ if (!function_exists('galopin_enqueue')){
 		
 		wp_register_script('jq-aim', get_template_directory_uri().'/js/min/jquery.aim.min.js', array('jquery'), $theme->get('Version'), true);
 		
-		wp_register_script('galopin', get_template_directory_uri().'/js/min/galopin.min.js', array('jquery'), $theme->get('Version'), true);
+		wp_register_script('galopin', get_template_directory_uri().'/js/galopin.js', array('jquery'), $theme->get('Version'), true);
+		
+		wp_enqueue_style( 'galopin-fonts', '//fonts.googleapis.com/css?family=Montserrat|Raleway:400,700&subset=latin,latin-ext');
 		
 		//main stylesheet
 		wp_enqueue_style('stylesheet', get_stylesheet_directory_uri().'/style.css', array(), $theme->get('Version'));
