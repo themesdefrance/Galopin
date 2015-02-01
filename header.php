141e54b0dc6e541f8ad8afb5aa43421f3c85cb18
Alex Borto <alex@themesdefrance.fr>
2015-02-01 14:14:15 +0100

    Fix masonry

--- header.php
+++ header.php
@@ -7,35 +7,35 @@
 <!--<![endif]-->
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>">
-	
+
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	
+
 	<link rel="shortcut icon" href="/favicon.ico?v=0">
-	
+
 	<meta name="viewport" content="width=device-width">
-	
+
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
-	
+
 	<!-- Scripts that need to be loaded first -->
 	<!--[if lt IE 9]>
 	<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
-	
+
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
 
 	<?php do_action('galopin_body_top'); ?>
-	
+
 	<div class="page-wrapper">
-		<div class="content-wrapper <?php if ((is_home() || is_front_page()) && get_option('galopin_use_hero')) echo 'cover'; ?>">
-			
+		<div class="content-wrapper <?php if ((is_front_page()) && get_option('galopin_use_hero')) echo 'cover'; ?>">
+
 			<header class="menu-wrapper">
 				<div class="search-wrapper">
 					<button class="form-toggle typcn typcn-zoom"></button>
-					 <?php get_search_form(true); ?> 
+					 <?php get_search_form(true); ?>
 				</div>
-				
+
 				<nav class="main-menu" role="navigation" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement">
 					<?php
 					wp_nav_menu(array(
@@ -47,31 +47,31 @@
 					));
 					?>
 				</nav>
-				
+
 				<ul class="social-menu">
 					<?php echo galopin_social(); ?>
 				</ul>
-				
+
 			</header><!--END .menu-wrapper-->
-			
+
 			<div class="content <?php if (galopin_is_masonry()) echo 'masonry-wrapper'; ?>">
-				
+
 				<div class="hero-image" style="background-image: url(<?php echo galopin_get_hero_image_url(); ?>);">
-				
+
 					<button class="menu-toggle typcn typcn-th-menu <?php if (get_option('galopin_dark_hero')) echo 'inverted'; ?>"></button>
-					
+
 					<?php if(get_option('galopin_hero_logo')) : ?>
-						
-						<a href="<?php echo home_url(); ?>" class="hero-logo">	
+
+						<a href="<?php echo home_url(); ?>" class="hero-logo">
 							<img src="<?php echo esc_url(get_option('galopin_hero_logo')); ?>" alt="<?php echo bloginfo('name'); ?>">
 						</a>
-					
+
 					<?php else: ?>
-					
-						<a href="<?php echo home_url(); ?>" class="hero-text">	
+
+						<a href="<?php echo home_url(); ?>" class="hero-text">
 							<?php echo apply_filters('galopin_hero_text', (get_option('galopin_hero_text') ? sanitize_text_field(get_option('galopin_hero_text')) : bloginfo('name'))); ?>
 						</a>
-					
+
 					<?php endif; ?>
-					
+
 				</div><!--END .hero-image-->
--- js/galopin.js
+++ js/galopin.js
@@ -2,121 +2,116 @@
 $(function(){
 	//initmasonry
 	$(document).ready(function(){
-		if ($('.masonry').length){
-			$('.masonry').imagesLoaded(function(){
-				//if ($('.masonry').length){
-					var masonry = new Masonry($('.masonry')[0], {
-						itemSelector: '.brick',
-						columnWidth: '.masonry .brick',
-						transitionDuration: '0.3s',
-						gutter: 30
-					});
-				//}
-			});
-		}
-	});
-	
-	//menu toggle on mobile view
-	$('.menu-toggle').click(function(){
-		$('.content-wrapper').toggleClass('menu-open');
-	});
-	
-	//menu aim
-	var useJsMenu = ($('.menu-wrapper .sub-menu').css('position') === 'absolute');
-	$('.main-menu').menuAim({
-		activate: function(row){
-			if (useJsMenu) $(row).find('> .sub-menu').show();
-		},
-		deactivate: function(row){
-			if (useJsMenu) $(row).find('> .sub-menu').hide();
-		},
-		exitMenu: function(){
-			if (useJsMenu) return true;
-		},
-		submenuDirection: 'below',
-		rowSelector: '> ul >li'
-	});
-	
-	//top bar search form
-	$('.search-wrapper .form-toggle').click(function(){
-		$(this).toggle();
-		$('.search-wrapper').addClass('active');
-		$('.search-wrapper .search-form').toggle();
-		$('.search-wrapper .search-field').focus();
-	});
-	
-	$('.search-wrapper .search-field').blur(function(event){
-		$('.search-wrapper').removeClass('active');
-		$('.search-wrapper .search-field').val('');
-		$('.search-wrapper .search-form').toggle();
-		$('.search-wrapper .form-toggle').toggle();
-	});
-	
-	var $menu = $('.home .menu-wrapper');
-	var winHeight = $(window).height(),
-		menuHeight = $menu.height(),
-		offset = $('#wpadminbar').height(),
-		heroImageStop = (winHeight-menuHeight-offset);
-	
-	if ($('.content-wrapper.cover').length){
-		//fit hero to the exact size
-		$('.cover .hero-image').css('height', heroImageStop+'px');
-		$menu.css('top', heroImageStop+'px');
-		
-		//fix the menu in hero mode
-		$(document).on('scroll', function(event){
-			var scroll = $(window).scrollTop();
-				
-			if (heroImageStop-scroll <= 0) $menu.addClass('stuck');
-			else if ($menu.hasClass('stuck')) $menu.removeClass('stuck');
+
+		var $container = $( '.masonry' );
+		$container.imagesLoaded( function(){
+			$container.masonry( {
+				itemSelector		: '.brick',
+				transitionDuration	: '0.3s'
+			} );
+		} );
+
+		//menu toggle on mobile view
+		$('.menu-toggle').click(function(){
+			$('.content-wrapper').toggleClass('menu-open');
 		});
-	}
-	
-	 $(document).ready(function(){
-	    $('.post-video, .widget-video, .entry-content').fitVids();
-	  });
-	
-	//back to top button
-	var $toTop = $('#back-to-top');
-	if ($(window).scrollTop() <= $(window).height()) $toTop.hide();
-	
-	$toTop.on('click', function(){
-		$('html,body').animate({
-			scrollTop: 0
-		}, 400);
-	});
-	
-	$(document).on('scroll', function(event){
-		if ($(window).scrollTop() > $(window).height()) $toTop.fadeIn();
-		else $toTop.fadeOut();
-	});
-	
-	//things dependant on window size
-	var resizeTimeout;
-	
-	function windowSizeChanged(){
-		//usingflyout menu or not?
-		var previousState = useJsMenu;
-		useJsMenu = ($('.menu-wrapper .sub-menu').css('position') === 'absolute');
-		
-		if (previousState != useJsMenu){
-			(useJsMenu) ? $('.menu-wrapper .sub-menu').hide() : $('.menu-wrapper .sub-menu').show();
-		}
-		
-		//hero image stuff
-		if ($('.content-wrapper.cover').length){
-			winHeight = $(window).height();
-			menuHeight = $menu.height();
-			offset = $('#wpadminbar').height();
+
+		//menu aim
+		var useJsMenu = ($('.menu-wrapper .sub-menu').css('position') === 'absolute');
+		$('.main-menu').menuAim({
+			activate: function(row){
+				if (useJsMenu) $(row).find('> .sub-menu').show();
+			},
+			deactivate: function(row){
+				if (useJsMenu) $(row).find('> .sub-menu').hide();
+			},
+			exitMenu: function(){
+				if (useJsMenu) return true;
+			},
+			submenuDirection: 'below',
+			rowSelector: '> ul >li'
+		});
+
+		//top bar search form
+		$('.search-wrapper .form-toggle').click(function(){
+			$(this).toggle();
+			$('.search-wrapper').addClass('active');
+			$('.search-wrapper .search-form').toggle();
+			$('.search-wrapper .search-field').focus();
+		});
+
+		$('.search-wrapper .search-field').blur(function(event){
+			$('.search-wrapper').removeClass('active');
+			$('.search-wrapper .search-field').val('');
+			$('.search-wrapper .search-form').toggle();
+			$('.search-wrapper .form-toggle').toggle();
+		});
+
+		var $menu = $('.home .menu-wrapper');
+		var winHeight = $(window).height(),
+			menuHeight = $menu.height(),
+			offset = $('#wpadminbar').height(),
 			heroImageStop = (winHeight-menuHeight-offset);
+
+		if ($('.content-wrapper.cover').length){
+			//fit hero to the exact size
 			$('.cover .hero-image').css('height', heroImageStop+'px');
 			$menu.css('top', heroImageStop+'px');
+
+			//fix the menu in hero mode
+			$(document).on('scroll', function(event){
+				var scroll = $(window).scrollTop();
+
+				if (heroImageStop-scroll <= 0) $menu.addClass('stuck');
+				else if ($menu.hasClass('stuck')) $menu.removeClass('stuck');
+			});
 		}
-	}
-	
-	$(window).resize(function(){
-		if (resizeTimeout) clearTimeout(resizeTimeout);
-		resizeTimeout = setTimeout(windowSizeChanged, 100);
+
+		$('.post-video, .widget-video, .entry-content').fitVids();
+
+
+		//back to top button
+		var $toTop = $('#back-to-top');
+		if ($(window).scrollTop() <= $(window).height()) $toTop.hide();
+
+		$toTop.on('click', function(){
+			$('html,body').animate({
+				scrollTop: 0
+			}, 400);
+		});
+
+		$(document).on('scroll', function(event){
+			if ($(window).scrollTop() > $(window).height()) $toTop.fadeIn();
+			else $toTop.fadeOut();
+		});
+
+		//things dependant on window size
+		var resizeTimeout;
+
+		function windowSizeChanged(){
+			//usingflyout menu or not?
+			var previousState = useJsMenu;
+			useJsMenu = ($('.menu-wrapper .sub-menu').css('position') === 'absolute');
+
+			if (previousState != useJsMenu){
+				(useJsMenu) ? $('.menu-wrapper .sub-menu').hide() : $('.menu-wrapper .sub-menu').show();
+			}
+
+			//hero image stuff
+			if ($('.content-wrapper.cover').length){
+				winHeight = $(window).height();
+				menuHeight = $menu.height();
+				offset = $('#wpadminbar').height();
+				heroImageStop = (winHeight-menuHeight-offset);
+				$('.cover .hero-image').css('height', heroImageStop+'px');
+				$menu.css('top', heroImageStop+'px');
+			}
+		}
+
+		$(window).resize(function(){
+			if (resizeTimeout) clearTimeout(resizeTimeout);
+			resizeTimeout = setTimeout(windowSizeChanged, 100);
+		});
 	});
 });
 })(jQuery);
