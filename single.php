5668a87a63db5edb2acd090d36f3e5c88fadcd2b
Alex Borto <alex@themesdefrance.fr>
2015-04-12 11:22:00 +0200

    Fix commentaires

--- single.php
+++ single.php
@@ -1,30 +1,30 @@
 <?php $sidebar = get_option('galopin_show_sidebar'); ?>
 <?php get_header(); ?>
 
-<?php do_action('galopin_before_main'); ?>	
+<?php do_action('galopin_before_main'); ?>
 
 <div class="wrapper">
-	
+
 	<?php do_action('galopin_top_main'); ?>
 
 	<div class="<?php if ($sidebar) echo 'grid'; ?>">
-	
+
 		<div class="main<?php if ($sidebar) echo ' col-2-3'; ?>" role="main" itemprop="mainContentOfPage">
-		
+
 			<?php the_post(); get_template_part('content', get_post_format()); ?>
-			
+
 			<?php galopin_single_post_nav(); ?>
-			
-			<?php comments_template(); ?>
+
+			<?php if(comments_open()) comments_template(); ?>
 
 		</div><!--END .main -->
-		
+
 		<?php if ($sidebar)get_sidebar('blog'); ?>
-		
+
 	</div><!--END .grid-->
-	
+
 	<?php do_action('galopin_bottom_main'); ?>
-	
+
 </div><!--END .wrapper -->
 
 <?php do_action('galopin_after_main'); ?>
